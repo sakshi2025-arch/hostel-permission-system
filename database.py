bb418771b968b91823cea3891b3cc7ee6336061b
SAKSHI CHAUGULE <sakshi2025-arch>
2026-05-12 23:13:47 +0530

    Updated chief panel UI

--- database.py
+++ database.py
@@ -1,23 +1,30 @@
 import sqlite3
 
 conn = sqlite3.connect('hostel.db')
+
 cur = conn.cursor()
 
 cur.execute("""
 CREATE TABLE IF NOT EXISTS requests (
+
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+
     student_name TEXT,
     branch TEXT,
     year TEXT,
     hostel_no TEXT,
     room_no TEXT,
+
     student_no TEXT,
     parent_no TEXT,
+
     outgoing_date TEXT,
     incoming_date TEXT,
+
     reason TEXT,
     emergency_level TEXT,
     emergency TEXT,
+
     status TEXT DEFAULT 'Pending'
 )
 """)
@@ -25,4 +32,4 @@ CREATE TABLE IF NOT EXISTS requests (
 conn.commit()
 conn.close()
 
-print("Database created successfully ✔")
+print("Database & Table Created Successfully ✅")
